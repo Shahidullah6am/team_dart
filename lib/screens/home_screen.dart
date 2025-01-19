53d6d64fb01dfeb890035b2446a30ac5dc424ddc
ashiqur <ashikur.6amtech@gmail.com>
2025-01-19 11:56:57 +0600

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,23 +11,83 @@ class HomeScreen extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: CustomScrollView(
           slivers: [
+
+            SliverToBoxAdapter(child: SizedBox(height: 50)),
             
             SliverToBoxAdapter(
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        // child: Image.asset(),
+                      ),
+                      SizedBox(width: 10),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Hello!'),
+                          SizedBox(height: 5),
+                          Text('Livia Vaccaro', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+                        ],
+                      ),
+                    ],
+                  ),
+
                   
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.black, width: 2),
-                    ),
-                   // child: Image.asset(),
-                  )
-                  
-                  
+                  Icon(Icons.notifications, color: Colors.black),
                 ],
               ),
-            )
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: 25)),
+
+
+            SliverToBoxAdapter(
+              child: Container(
+                padding: EdgeInsets.all(18),
+                height: 150,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text('Your todays task \n almost done!', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.white)),
+                        SizedBox(height: 25),
+                        
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Text("View Task",),
+                        )
+                        
+                      ],
+                    ),
+                    
+                  ],
+                ),
+              )
+            ),
+
+
+
+
             
           ],
         ),
