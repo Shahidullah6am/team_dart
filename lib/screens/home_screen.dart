7c9f764bb7ddbb43e66ac966fdef09c2f9deacfa
ashiqur <ashikur.6amtech@gmail.com>
2025-01-19 11:40:59 +0600

    homepage init

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,32 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: CustomScrollView(
+          slivers: [
+            
+            SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.black, width: 2),
+                    ),
+                   // child: Image.asset(),
+                  )
+                  
+                  
+                ],
+              ),
+            )
+            
+          ],
+        ),
+      )
+    );
   }
 }
